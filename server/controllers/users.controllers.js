072f37c5a01a46b2d6bde526474ceab51b05b3b6
abel-hb <abelherrero.17@campuscamara.es>
2020-01-21 02:52:07 +0100

    [COMMIT]UPDATE CODE FAIL

--- server/controllers/users.controllers.js
+++ server/controllers/users.controllers.js
@@ -2,16 +2,17 @@ const User = require('../models/users');
 
 const usersCtrl = {};
 
-usersCtrl.getUsers = async (req,res) => {
+usersCtrl.getUsers = async (req, res) => {
    const users = await User.find();
    res.json(users);
 }
 
-usersCtrl.getUser = function (){
-
+usersCtrl.getUser = async (req, res) => {
+    const user = await User.findById(req,params.id);
+    res.json(user);
 }
 
-usersCtrl.createUser = async (req,res) => {
+usersCtrl.createUser = async (req, res) => {
     const user = new User(req.body);
     await user.save();
     res.json({
@@ -19,13 +20,25 @@ usersCtrl.createUser = async (req,res) => {
     });
 };
 
-usersCtrl.editUser = function (){
+usersCtrl.editUser = async (req, res) =>{
+    const { id } = req.params;
+    const user = {
+        name = req.body.name,
+        surname = req.body.surname,
+        dni = req.body.dni,
+        email = req.body.email,
+        tlf = req.body.tlf,
+        transport = req.body.transport,
+        discharge_date = req.body.discharge_date,
+    }
+    await User.findByIdAndUpdate(id, {$set:user}, {new: true});
+    res.json({status:'User Update'})
+};
 
-}
-
-usersCtrl.deleteUser = function (){
-
-}
+usersCtrl.deleteUser = async (req, res) =>{
+    await User.findByIdAndDelete(req.params.id);
+    res.json({status:'User Deleted'})
+};
 
 
 module.exports = usersCtrl;
