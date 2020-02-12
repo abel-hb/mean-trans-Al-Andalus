c28d32e28169ca83166cd8160b24e1b6209a989c
abel-hb <abelherrero.17@campuscamara.es>
2020-02-12 21:02:22 +0100

    [COMMIt] ADD CODE APP MODULE

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -10,6 +10,12 @@ import { TransportComponent } from './components/transport/transport.component';
 import { AplicationComponent } from './components/aplication/aplication.component';
 import { PlacesComponent } from './components/places/places.component';
 import { FormsModule } from '@angular/forms';
+import { TypeTransService } from './services/type-trans.service';
+import { TransportService } from './services/transport.service';
+import { UsersService } from './services/users.service';
+import { PlacesServices } from './services/places.service';
+import { AplicationService } from './services/aplication.service';
+
 import { HttpClientModule } from '@angular/common/http';
 
 
@@ -28,7 +34,7 @@ import { HttpClientModule } from '@angular/common/http';
     FormsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [TypeTransService, TransportService,UsersService,PlacesServices, AplicationService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
